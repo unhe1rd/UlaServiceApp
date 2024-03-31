0d92bd562488823454169bc4a98df98cffd5a972
Mikhail Ulanov <mixaulanoff@yandex.ru>
2024-03-31 20:02:23 +0300

    launch screen fixes

--- UlaServiceApp/UlaServiceApp/Modules/SplashModule/SplashScreenView.swift
+++ UlaServiceApp/UlaServiceApp/Modules/SplashModule/SplashScreenView.swift
@@ -1,62 +1,35 @@
 //
-//  SplashScreenView.swift
+//  Background.swift
 //  UlaServiceApp
 //
 //  Created by Mike Ulanov on 31.03.2024.
 //
 
+
 import SwiftUI
 
-
-struct AnimatedGradientView: View {
-    let durationTime: TimeInterval
-    @Binding var currentColors: [Color]
-
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var opacity = 0.5
+    
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: currentColors), startPoint: .top, endPoint: .bottom)
-            .animation(Animation.linear(duration: durationTime).repeatForever())
-            .onAppear {
-                withAnimation{
-                    currentColors.reverse()
+        if isActive{
+        } else {
+            VStack{
+                VStack{
+                    BackgroundView(imageOnBackground: true, durationTime: 2)
+                }
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 2)){
+                        self.opacity = 1.0
+                    }
                 }
             }
-            .ignoresSafeArea()
-    }
-}
-
-
-struct AnimatingGradientBuilder: View {
-    @State private var currentColors: [Color]
-    private let durationTime: TimeInterval
-
-    init(backgroundColors: [Color], durationTime: TimeInterval) {
-        self._currentColors = State(initialValue: backgroundColors)
-        self.durationTime = durationTime
-    }
-
-    var body: some View {
-        ZStack {
-            AnimatedGradientView(durationTime: durationTime, currentColors: $currentColors)
-        }
-    }
-}
-
-struct BackgroundView: View {
-    @Environment(\.colorScheme) var colorScheme
-    var imageOnBackground: Bool
-    var durationTime: Int
-    var body: some View {
-        ZStack{
-            if colorScheme == .dark{
-                AnimatingGradientBuilder(backgroundColors: [Color.purple, Color.black], durationTime: TimeInterval(durationTime))
-            } else {
-                AnimatingGradientBuilder(backgroundColors: [Color.purple, Color.mint], durationTime: TimeInterval(durationTime))
-            }
-            if imageOnBackground == true {
-                Image("backgroundImage")
-                   .resizable()
-                   .frame(width: 400, height: 400)
-                   .shadow(color: .white, radius: 5)
+            .onAppear(){
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                    self.isActive = true
+                }
             }
         }
     }
@@ -64,5 +37,5 @@ struct BackgroundView: View {
 
 
 #Preview {
-    BackgroundView(imageOnBackground: true, durationTime: 2)
+    SplashScreenView()
 }
